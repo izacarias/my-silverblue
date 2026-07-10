4934efe3ec75322142210fe4d2094fb6debaae42
izacarias <i.zacarias@tu-bs.de>
2026-07-10 15:35:10 +0200

    chore: Change base image and add dnf packages

--- Containerfile
+++ Containerfile
@@ -1,18 +1,40 @@
+ARG BASE_IMAGE_NAME="silverblue"
+ARG FEDORA_MAJOR_VERSION="44"
+ARG BASE_IMAGE="quay.io/fedora-ostree-desktops/silverblue"
+ARG BASE_IMAGE_REF="${BASE_IMAGE}:${FEDORA_MAJOR_VERSION}"
+ARG BREW_IMAGE="ghcr.io/ublue-os/brew:latest"
+
+FROM ${BREW_IMAGE} AS brew
+
 FROM scratch AS ctx
 
 # Copy folders to the target image
-COPY build_files /
-COPY system_files /system_files
+COPY /system_files /system_files
+COPY /build_files /build_files
+COPY --from=brew /system_files /system_files/shared
 
 # Base Image
-FROM quay.io/fedora/fedora-bootc:44 
+FROM ${BASE_IMAGE_REF} AS base-common
 
 # Modify the base system (add my software and configs)
-RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
-    --mount=type=cache,dst=/var/cache \
-    --mount=type=cache,dst=/var/log \
-    --mount=type=tmpfs,dst=/tmp \
-    /ctx/build.sh
+RUN --mount=type=cache,dst=/var/cache/libdnf5 \
+    --mount=type=cache,dst=/var/cache/rpm-ostree \
+    --mount=type=bind,from=ctx,source=/build_files,target=/ctx/build_files \
+#    --mount=type=bind,from=ctx,source=/image-versions.yml,target=/ctx/image-versions.yml \
+#    --mount=type=secret,id=GITHUB_TOKEN \
+    --mount=type=tmpfs,dst=/boot \
+    bash -euo pipefail -c ' \
+        dnf5 config-manager setopt keepcache=1 && \
+        dnf5 config-manager setopt install_weak_deps=0 && \
+        /ctx/build_files/base/03-packages.sh && \
+        /ctx/build_files/base/05-override-install.sh \
+    '
 
-RUN rpm-ostree cleanup -m && \
-    ostree container commit
+# Makes `/opt` writeable by default
+# Needs to be here to make the main image build strict (no /opt there)
+# This is for downstream images/stuff like k0s
+RUN rm -rf /opt && ln -s /var/opt /opt
+
+CMD ["/sbin/init"]
+
+RUN bootc container lint --fatal-warnings
---
+++ build_files/base/03-packages.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/bash
+
+echo "::begin_group:: ===$(basename "$0")==="
+
+set -ouex pipefail
+
+# DNF-related operations should be done here whenever possible
+
+FEDORA_PACKAGES = (
+     adw-gtk3-theme
+     adwaita-fonts-all
+     alsa-firmware
+     alsa-tools-firmware
+     bootc
+     containerd
+     ddcutil
+     distrobox
+     evtest
+     fastfetch
+     firewall-config
+     fish
+     flatpak-spawn
+     gcc
+     gcc-c++
+     git-credential-libsecret
+     gnome-tweaks
+     gnupg2-scdaemon
+     google-noto-sans-cjk-vf-fonts
+     gum
+     gvfs-nfs
+     ibus-mozc
+     ibus-unikey
+     igt-gpu-tools
+     input-remapper
+     just
+     libappindicator-gtk3
+     libayatana-appindicator-gtk3
+     libcamera-gstreamer
+     libcamera-tools
+     libratbag-ratbagd
+     libva-utils
+     libxcrypt-compat
+     make
+     mesa-libGLU
+     mozc
+     nautilus-gsconnect
+     openrgb-udev-rules
+     openssh-askpass
+     pipewire-libs-extra
+     switcheroo-control
+     waypipe
+     wireguard-tools
+     wl-clipboard
+     xdg-terminal-exec
+     zenity
+     zsh
+)
+
+# Install Fedora, Tailscale, and multimedia packages together while keeping COPR packages isolated.
+echo "Installing ${#FEDORA_PACKAGES[@]} Fedora packages plus Tailscale and multimedia packages..."
+# dnf5 config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+# dnf5 config-manager setopt tailscale-stable.enabled=0
+dnf5 -y install \
+    -x PackageKit* \
+    "${FEDORA_PACKAGES[@]}" \
+    ffmpeg{,-libs} libavcodec @multimedia gstreamer1-plugins-{bad-free,bad-free-libs,good,base} lame{,-libs} libfdk-aac libjxl ffmpegthumbnailer
+
+## Pins and Overrides
+## Use this section to pin packages in order to avoid regressions
+# Remember to leave a note with rationale/link to issue for each pin!
+#
+# Example:
+#if [ "$FEDORA_MAJOR_VERSION" -eq "41" ]; then
+#    Workaround pkcs11-provider regression, see issue #1943
+#    rpm-ostree override replace https://bodhi.fedoraproject.org/updates/FEDORA-2024-dd2e9fb225
+#fi
+
+echo "::end_group:: ===$(basename "$0")==="
